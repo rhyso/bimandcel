57fc45b2700bbd9ffbfa9d83c09cc086047fa75f
rhyso <rhyswork@gmail.com>
2018-05-04 13:58:05 +0100

    Updated section styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Location} from './components/location.jsx';
 import flower from './images/flower1.png'
-import champ from './images/champ-tran.jpg'
+import amp from './images/noun_840428_cc.png'
 
 import classNames from 'classnames';
 
@@ -19,8 +19,15 @@ class App extends Component {
         <div className="content">
           <div className={classNames('big-div', 'title-area')}>
               <div className={classNames('inner-div', 'flower-div')}>
-                  <img src={flower} className="decorative-flower"/>
-                  <h1>Celyn Lloyd Thomas & Imogen Walker</h1>
+                  <div className="entrance-text">
+                     <img src={flower} className="decorative-flower"/>
+                     <h1 className="name-one">Imogen Mary Charlotte Walker</h1>
+                     <br/>
+                     <img src={amp} className="amp"/>
+                     <br/>
+                     <h1 className="name-two">Celyn Lloyd Thomas</h1>
+                     <h2 className="wedding-date">15.09.2018</h2>
+                  </div>
                   <img src={flower} className="decorative-flower2"/>
               </div>
           </div>
@@ -32,6 +39,18 @@ class App extends Component {
           <div className={classNames('big-div', 'details')}>
               <div className="inner-div">
                 <h2>DETAILS</h2>
+                  <div className="inner-content">
+                      <p>Imogen Mary Charlotte Walker and Celyn Lloyd Thomas warmly invite you to their wedding on Saturday 15 September
+
+                          Ceremony at 12 noon at the Catholic Parish of St Mary’s, Bulwark Road, Chepstow, NP16 5JE
+
+                          Followed by a reception near Gaerllwyd, NP16 6BA (map and directions included)
+                          <br/>
+                          Parking at the church and the field
+
+                          We’ll be spending a lot of the day in a field so sensible shoes and warm jumpers are advised.
+                      </p>
+                  </div>
               </div>
           </div>
 
@@ -39,8 +58,12 @@ class App extends Component {
           <div className={classNames('big-div', 'location')}>
               <div className="inner-div">
                 <h2>LOCATION</h2>
-                  <h3>Ceremony at 12 noon at the Catholic Parish of St Mary’s, Bulwark Road, Chepstow, NP16 5JE
-                  </h3>
+                  <div className="inner-content">
+
+                      <p>Ceremony at 12 noon at the Catholic Parish of St Mary’s, Bulwark Road, Chepstow, NP16 5JE</p>
+                      {/*<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1mFZXZraFWcllwCRy6bNDoO78A7FplXHn" width="640" height="480"></iframe>*/}
+
+                  </div>
                   <Location />
               </div>
           </div>
@@ -49,52 +72,89 @@ class App extends Component {
           <div className={classNames('big-div', 'accomodation')}>
               <div className="inner-div">
                   <h2>ACCOMODATION</h2>
-                  <h3>Camping:
-                      Hotels:
-                      Beaufort Hotel 2* - Beaufort Square Chepstow NP16 5EP
-                      Castle View Hotel 3* - 16 Bridge Street, Chepstow NP16 5EZ
-                      Racecourse Guest House 5* - St Lawrence Road, Chepstow NP16 6BE
-                      St Pierre Park Hotel 4* - St Pierre Park, Chepstow, NP16 6YA
-                      Stoulgrove Country House 5* - Stoulgrove Lane, Chepstow NP16 7QE
-                      Lynchgate 5*, 47 Church Road, Caldicot, NP26 4HQ
-                      Northgate House, Caerwent
-                      Coach and Horses, Caerwent
-                      The First Hurdle, Chepstow
-                      Hampton by Hilton - Magor
-                      Brooklands Motel - Portskewett
-                      The George Hotel - Chepstow
-                      The Huntsman - Shirenewton
-                      Willowbrook - Pwllmeyric
-                      47a Townhouse B&B - Chepstow
-                      Days Inn - Magor
+                  <div className="inner-content">
+
+                  <ul>
+                      <li>Hotels:</li>
+                      <li>Beaufort Hotel 2* - Beaufort Square Chepstow NP16 5EP</li>
+                      <li>Castle View Hotel 3* - 16 Bridge Street, Chepstow NP16 5EZ</li>
+                      <li>Racecourse Guest House 5* - St Lawrence Road, Chepstow NP16 6BE</li>
+                      <li>St Pierre Park Hotel 4* - St Pierre Park, Chepstow, NP16 6YA</li>
+                      <li>Stoulgrove Country House 5* - Stoulgrove Lane, Chepstow NP16 7QE</li>
+                      <li>Lynchgate 5*, 47 Church Road, Caldicot, NP26 4HQ</li>
+                      <li>Northgate House, Caerwent</li>
+                      <li>Coach and Horses, Caerwent</li>
+                      <li>The First Hurdle, Chepstow</li>
+                      <li>Hampton by Hilton - Magor</li>
+                      <li>Brooklands Motel - Portskewett</li>
+                      <li>The George Hotel - Chepstow</li>
+                      <li>The Huntsman - Shirenewton</li>
+                      <li>Willowbrook - Pwllmeyric</li>
+                      <li>47a Townhouse B&B - Chepstow</li>
+                      <li>Days Inn - Magor</li>
+                  </ul>
 
                       There are also a selection of Air BnBs nearby
-                  </h3>
+                  </div>
               </div>
           </div>
+
 
 
           <div className={classNames('big-div', 'transport')}>
               <div className="inner-div">
                 <h2>TRANSPORT</h2>
-                  <h3>There will be a minibus on hand taking guests from the reception back to the centre of Chepstow (or to their hotel within reason)
+                  <div className="inner-content">
+                  <p>There will be a minibus on hand taking guests from the reception back to the centre of Chepstow (or to their hotel within reason)
 
                       Please let us know in your RSVP whether you need transport from the church to the reception or whether you are happy making your own way. You can park your cars in the field.
 
-                      To RSVP please email XXXX
-                  </h3>
+                  </p>
+                  </div>
               </div>
           </div>
 
           <div className={classNames('big-div', 'social')}>
               <div className="inner-div">
-                <h2>SOCIAL</h2>
+                <h2>RSVP</h2>
+                  <div className="inner-content">
+                      <p>To RSVP please email XXXX</p>
+
+                  </div>
               </div>
           </div>
 
           <div className={classNames('big-div', 'thanks')}>
               <div className="inner-div">
-                <h2>THANKS</h2>
+                <h2>Luminary</h2>
+                  <div className="inner-content">
+                      <p>
+                          My luminary,
+                          my morning and evening
+                          star. My light at noon
+                          when there is no sun
+                          and the sky lowers. My balance
+                          of joy in a world
+                          that has gone off joy's
+                          standard. Yours the face
+                          that young I recognised
+                          as though I had known you
+                          of old. Come, my eyes
+                          said, out into the morning
+                          of a world whose dew
+                          waits for your footprint.
+                          Before a green altar
+                          with the thrush for priest
+                          I took those gossamer
+                          vows that neither the Church
+                          could stale nor the Machine
+                          tarnish, that with the years
+                          have grown hard as flint,
+                          lighter than platinum
+                          on our ringless fingers.
+
+                      </p>
+                  </div>
               </div>
           </div>
         <p className="App-intro">
