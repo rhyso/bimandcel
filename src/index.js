70d0f19300108fb34e5ec9d18cdb4da4ece898eb
rhyso <rhyswork@gmail.com>
2018-06-04 15:07:57 +0100

    raf pollyfill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
+import 'raf/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
