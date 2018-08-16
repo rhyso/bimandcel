f6d731069b3f725f6a6bbf066bf03048c4390930
rhyso <rhyswork@gmail.com>
2018-08-16 17:09:07 +0100

    tweaks

--- src/components/updates.jsx
+++ src/components/updates.jsx
@@ -14,27 +14,22 @@ export class Updates extends Component {
                     <h2>UPDATES</h2>
                     <div className="inner-content">
 
+                        <p className="special-heading"> <h3>WE'RE NEARLY GETTING MARRIED!</h3></p>
 
-
-                                <div>
-
-                                    <h3></h3>
+                            <div className="l-r-wrapper">
+                                    <h3>1.</h3>
                                     <p>
-
-                                        <b>WE'RE NEARLY GETTING MARRIED!</b><br/>
 
                                         We are so excited to see you all. Here are a couple of reminders ahead of the day:
                                          Bring cash with you for the bar
 
                                     </p>
 
-                                </div>
+                            </div>
 
-                                <br/>
 
-                                <div>
-
-                                    <h3></h3>
+                            <div className="l-r-wrapper">
+                                    <h3>2. </h3>
                                     <p>
 
                                               Make sure you've organised or thought about taxis or lifts in cars to get to and from the field -
@@ -43,27 +38,25 @@ export class Updates extends Component {
 
                                     </p>
 
-                                </div>
+                            </div>
 
-                                <br />
 
-                                <div>
 
-                                    <h3></h3>
+                            <div className="l-r-wrapper">
+                                    <h3>3.</h3>
                                     <p>
                                             Bring a warm jumper in case it gets chilly in the evening and a brolly if it looks like rain.
                                             The bar in the field won't be stocking any spirits (although there will be lots of beer, wine and cider!)
                                             there will be free mixers at the bar so feel free to bring a spirit if you wish.
-                                    </p>
                                     <br />
-                                    <p>Have also added some more information about <a href='#section-camping'> camping here </a></p>
+                                        <br />
+                                    Have also added some more information about <a href='#section-camping'> camping here </a></p>
 
-                                </div>
-
-
+                             </div>
 
                     </div>
                 </div>
+
             </div>
 
         )
